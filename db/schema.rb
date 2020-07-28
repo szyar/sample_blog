a5b772d64e423d736e0aed179bea75d03331ced6
szyar <soezayarszyar@gmail.com>
2020-07-28 09:48:38 +0630

    adding blog model

--- db/schema.rb
+++ db/schema.rb
@@ -16,6 +16,8 @@ ActiveRecord::Schema.define(version: 2020_07_28_021034) do
   enable_extension "plpgsql"
 
   create_table "blogs", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
   end
 
 end
