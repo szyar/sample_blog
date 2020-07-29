fda35e732bb74f118b311a4b2dbe63fa73f8b8f8
szyar <soezayarszyar@gmail.com>
2020-07-29 09:23:08 +0630

    adding blogs function view contents

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,11 +1,16 @@
 class BlogsController < ApplicationController
   def index
+    @blogs = Blog.all
   end
   def new
     @blog =Blog.new
   end
   def create
-    Blog.create(title: params[:blog][:title], content: params[:blog][:content])
+    Blog.create(blog_params)
     redirect_to new_blog_path
+  end
+  private
+  def blog_params
+    params.require(:blog).permit(:title, :content)
   end
 end
