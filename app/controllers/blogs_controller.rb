e974a766d06482c3153b55c1bc6ea361778c7129
szyar <soezayarszyar@gmail.com>
2020-07-29 10:40:20 +0630

    adding validation

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -2,6 +2,7 @@ class BlogsController < ApplicationController
   def index
     @blogs = Blog.all
   end
+
   def show
     @blog = Blog.find(params[:id])
   end
@@ -9,10 +10,16 @@ class BlogsController < ApplicationController
   def new
     @blog =Blog.new
   end
+
   def create
-    Blog.create(blog_params)
-    redirect_to new_blog_path
+    @blog = Blog.new(blog_params)
+    if @blog.save
+      redirect_to blogs_path, notice: "You have created a blog!"
+    else
+      render :new
+    end
   end
+
   private
   def blog_params
     params.require(:blog).permit(:title, :content)
