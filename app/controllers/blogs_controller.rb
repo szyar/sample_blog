1054907b075f538fec97e9a8b949dfe4b92963d7
szyar <soezayarszyar@gmail.com>
2020-07-29 09:58:52 +0630

    adding blog detail page

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -2,6 +2,10 @@ class BlogsController < ApplicationController
   def index
     @blogs = Blog.all
   end
+  def show
+    @blog = Blog.find(params[:id])
+  end
+
   def new
     @blog =Blog.new
   end
